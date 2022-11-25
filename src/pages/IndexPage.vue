b848678ef6b316a05751e55b7ce9818262561918
Amir <amirrezwanoori@gmail.com>
2022-11-25 10:26:59 +0330

    first init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div class="colums text-center">
+      <h2>Welcome to</h2>
+      <h1>MyMassenger</h1>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts"></script>
